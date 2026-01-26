2d434d789a3b15caf1d110365fbfd9157e702e54
pyr <jzhangnv@gmail.com>
2026-01-26 01:01:46 -0500

    Removed comments (tree is perfectly fine)

--- BST.js
+++ BST.js
@@ -17,16 +17,16 @@ class Tree {
             }
         return Object.values(obj);
     }
-    buildTree(array = this.arr){ // Doesn't sort correctly
+    buildTree(array = this.arr){
         if (array.length == 0) return null;
         if (array.length == 1) return array[0];
         let sortedArray = this.#sortArr(array);
         let medianIndex = Math.floor(sortedArray.length/2);
         let median = sortedArray[medianIndex];
-        let right = sortedArray.slice(medianIndex+1); // On recursion it'll use the middle one
-        let left = sortedArray.slice(0, medianIndex); // Not the first one which was sorted
+        let right = sortedArray.slice(medianIndex+1);
+        let left = sortedArray.slice(0, medianIndex);
         let test = new Node(median, this.buildTree(left), this.buildTree(right))
         return test;
     }
 }
-console.log(new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]).buildTree());
+console.log(new Tree([1, 5, 9, 14, 23, 27]).buildTree());
